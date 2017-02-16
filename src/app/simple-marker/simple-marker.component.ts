6252e4ab73be4af4912a6c3c659228306dcfb425
andrew rodgers <rodgers.andrew14@gmail.com>
2017-02-15 18:20:39 -0700

    added node modules and made changes to the simple marker component

--- src/app/simple-marker/simple-marker.component.ts
+++ src/app/simple-marker/simple-marker.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { Ng2MapComponent } from 'ng2-map';
 
 @Component({
   selector: 'app-simple-marker',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimpleMarkerComponent implements OnInit {
 
-  constructor() { }
+	@ViewChild( Ng2MapComponent ) ng2MapComponent: Ng2MapComponent;
+	public map: google.maps.Map;
+	
+	ngOnInit() {
+	  	this.ng2MapComponent.mapReady$.subscribe(map => {
+    		this.map = map;
+    	})
+  	}
 
-  ngOnInit() {
+  constructor() { 
+  	 Ng2MapComponent['apiUrl'] = 'https://maps.google.com/maps/api/js?key=AIzaSyCX39r8r3Zf5VGvKfKCjWLgfA0SFmJufPM';
   }
-
 }
+
